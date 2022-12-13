a4192a401f105160b696145f683912d7f0cd3839
Luka Sever <severluka045@gmail.com>
2022-12-13 19:35:32 +0100

    update

--- d13/13.py
+++ d13/13.py
@@ -9,45 +9,33 @@ with open('C:/Users/Luka/Documents/aoc/d13/example.txt') as f:
         data[-1].append(json.loads(line))
 
 
-def is_correctly_sorted(list1, list2):  # compare lists
-    print("Comparing lists", list1, "|AND|", list2)
-    unresolved_int_comparing = True
+def list_cmp(list1, list2): # ret 1 -> success, ret 0 -> they're equal, ret -1 -> fail, should propagate immediately to the end and return false
+    equal = True
     for i in range(min(len(list1), len(list2))):
+        # if they're both of type int
         if type(list1[i]) == int and type(list2[i]) == int:
-            print(f"Comparing {list1[i]} and {list2[i]}, both integers.")
             if list1[i] > list2[i]:
-                print("Left side bigger. Returning false.")
-                return False
-            elif list1[i] < list2[i]:
-                print("Right side bigger. Remembering for later so that we know whether we need a tie breaker or not - list size.")
-                unresolved_int_comparing = False
-
-        # xor of types, if one is list and the other isn't
-        elif type(list1[i]) != type(list2[i]):
-            if type(list1[i]) == int:
-                if not is_correctly_sorted([list1[i]], list2[i]):
-                    return False
-            else:
-                if not is_correctly_sorted(list1[i], [list2[i]]):
-                    return False
-        else: # both lists
-            if not is_correctly_sorted(list1[i], list2[i]):
-                return False
-
-    if unresolved_int_comparing:
-        print("Unresolved int comparing. Determining result based on list size.")
-        if len(list1) > len(list2):
-            print("Left side bigger. Returning false.")
-            return False
+                return -1
+            elif list1[i] != list2[i]:
+                equal = False
+        # if they're both of type list
+        elif type(list1[i]) == list and type(list2[i]) == list:
+            ret = list_cmp(list1[i], list2[i])
+            if ret == -1:
+                return -1
+            elif ret == 0:
+                return -1 if len(list1[i]) > len(list2[i]) else 0
+        else: # different types, int and list or list and int
+            
 
 
-    return True
+
 
 correctly_sorted = []
 c = 0
 for i in range(1, len(data) + 1):
     print("---------------------------------------------------")
-    if is_correctly_sorted(*data[i - 1]):
+    if list_cmp(*data[i - 1]):
         c += i
         correctly_sorted.append(i)
         print(i)
