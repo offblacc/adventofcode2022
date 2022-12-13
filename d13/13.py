a292492b815965b1b306d4b1877e9df169388384
Luka Sever <severluka045@gmail.com>
2022-12-14 00:45:47 +0100

    13 gotov

--- d13/13.py
+++ d13/13.py
@@ -1,44 +1,76 @@
-import json
-
+import json, copy
 data = [[]]
-with open('C:/Users/Luka/Documents/aoc/d13/example.txt') as f:
+with open('input.txt') as f:  # C:/Users/Luka/Documents/aoc/d13/
     for line in [x.strip() for x in f]:
         if line == '':
             data.append([])
             continue
         data[-1].append(json.loads(line))
+data0 = copy.deepcopy(data)
 
-
-def list_cmp(list1, list2): # ret 1 -> success, ret 0 -> they're equal, ret -1 -> fail, should propagate immediately to the end and return false
-    equal = True
-    for i in range(min(len(list1), len(list2))):
-        # if they're both of type int
+def is_correctly_sorted(list1, list2):
+    i = 0
+    while i < min(len(list1), len(list2)):
         if type(list1[i]) == int and type(list2[i]) == int:
-            if list1[i] > list2[i]:
+            if list1[i] < list2[i]:
+                return 1
+            elif list1[i] > list2[i]:
                 return -1
-            elif list1[i] != list2[i]:
-                equal = False
-        # if they're both of type list
         elif type(list1[i]) == list and type(list2[i]) == list:
-            ret = list_cmp(list1[i], list2[i])
-            if ret == -1:
+            if len(list2[i]) == 0 and len(list1[i]) != 0:
                 return -1
-            elif ret == 0:
-                return -1 if len(list1[i]) > len(list2[i]) else 0
-        else: # different types, int and list or list and int
-            
 
+            if len(list1[i]) == 0 and len(list2[i]) != 0:
+                return 1
 
+            if is_correctly_sorted(list1[i], list2[i]) == 1:
+                return 1
 
+            if is_correctly_sorted(list1[i], list2[i]) == -1:
+                return -1
 
-correctly_sorted = []
+        elif type(list1[i]) != type(list2[i]):
+            if type(list1[i]) == int:
+                list1[i] = [list1[i]]
+            else:
+                list2[i] = [list2[i]]
+            i -= 1
+        i += 1
+    if len(list1) < len(list2):
+        return 1
+    elif len(list1) > len(list2):
+        return -1
+    else:
+        return 0
 c = 0
 for i in range(1, len(data) + 1):
-    print("---------------------------------------------------")
-    if list_cmp(*data[i - 1]):
+    if is_correctly_sorted(*data[i - 1]) == 1:
         c += i
-        correctly_sorted.append(i)
-        print(i)
-print('-------------------')
 print(c)
-print(*correctly_sorted, sep = ' ')
+
+# -- clumsy
+data, data2 = data0, []
+for elem in data:
+    data2.append(elem[0])
+    data2.append(elem[1])
+data = data2
+data.append([[2]])
+data.append([[6]])
+
+changes = True
+while changes:
+    changes = False
+    for i in range(len(data) - 1):
+        if is_correctly_sorted(data[i], data[i + 1]) == -1:
+            data[i], data[i + 1] = data[i + 1], data[i]
+            changes = True
+
+prod = 1
+for i in range(len(data)):
+    line = str(data[i])
+    if ',' in line: continue
+    if '[2]' in line:
+        prod *= i + 1
+    elif '[6]' in line:
+        prod *= i + 1
+print(prod)
